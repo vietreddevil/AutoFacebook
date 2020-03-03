ac8bf2165651f213f341047e7a5a8c2f7c8c4f14
Tran Viet <reddevil241097@gmail.com>
2020-03-03 11:48:25 +0700

    d

---
+++ AutoFacebook/CustomObj.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoFacebook
+{
+    public class CustomObj
+    {
+        private List<Nick> _nicks;
+        public List<Nick> Nicks
+        {
+            get { return _nicks; }
+            set { _nicks = value; }
+        }
+
+        public CustomObj()
+        {
+            _nicks = new List<Nick>();
+        }
+    }
+
+    public class Nick
+    {
+        //private string _id;
+        //public string ID
+        //{
+        //    get { return _id; }
+        //    set { _id = value; }
+        //}
+
+        private string _username;
+        public string Username
+        {
+            get { return _username; }
+            set { _username = value; }
+        }
+
+        private string _password;
+        public string Password
+        {
+            get { return _password; }
+            set { _password = value; }
+        }
+
+        private CookieObj _cookie;
+        public CookieObj Cookie
+        {
+            get { return _cookie; }
+            set { _cookie = value; }
+        }
+
+        public Nick()
+        {
+            CookieObj _cookie = new CookieObj(); ;
+            Cookie = _cookie;
+            Password = "";
+            Username = "";
+        }
+    }
+
+    public class CookieObj
+    {
+        public CookieObj()
+        {
+            SB = "";
+            FR = "";
+            C_User = "";
+            DATR = "";
+            XS = "";
+        }
+        private string _sb;
+        public string SB
+        {
+            get { return _sb; }
+            set { _sb = value; }
+        }
+
+        private string _xs;
+        public string XS
+        {
+            get { return _xs; }
+            set { _xs = value; }
+        }
+
+        private string c_user;
+        public string C_User
+        {
+            get { return c_user; }
+            set { c_user = value; }
+        }
+
+        private string _fr;
+        public string FR
+        {
+            get { return _fr; }
+            set { _fr = value; }
+        }
+
+        private string _datr;
+        public string DATR
+        {
+            get { return _datr; }
+            set { _datr = value; }
+        }
+    }
+}
--- AutoFacebook/Program.cs
+++ AutoFacebook/Program.cs
@@ -13,25 +13,26 @@ namespace AutoFacebook
 {
     class Program
     {
+        public static int[] ThreadCookieSeq = { 0, 0 };
         static async Task Main(string[] args)
         {
-            dynamic cookie;
+            CustomObj cookie;
             using(StreamReader r = new StreamReader("../../cookie.json"))
             {
                 string json = r.ReadToEnd();
-                cookie = JsonConvert.DeserializeObject<dynamic>(json);
+                cookie = JsonConvert.DeserializeObject<CustomObj>(json);
             }
-            //Console.WriteLine(cookie.nicks);
-            Thread ts = new Thread(async () => await LoginFacebook(cookie, "sypvudn_bowersescu_1533740491@tfbnw.net", "m1yfrtkg1eu", 0));
+
+            Thread ts = new Thread(async () => await FacebookTool(cookie, "sypvudn_bowersescu_1533740491@tfbnw.net", "m1yfrtkg1eu", 0));
             ts.Start();
 
-            Thread ts1 = new Thread(async () => await LoginFacebook(cookie, "dcxjatc_putnamstein_1533740513@tfbnw.net", "nj77rawk5yc", 1));
+            Thread ts1 = new Thread(async () => await FacebookTool(cookie, "dcxjatc_putnamstein_1533740513@tfbnw.net", "nj77rawk5yc", 1));
             ts1.Start();
 
             Console.ReadKey();
         }
 
-        static async Task<int> LoginFacebook(dynamic cookie, string username, string password, int threadSeq)
+        static async Task<int> FacebookTool(CustomObj cookie, string username, string password, int threadSeq)
         {
             ChromeOptions op = new ChromeOptions();
             var set = new ChromeMobileEmulationDeviceSettings();
@@ -51,14 +52,118 @@ namespace AutoFacebook
             op.AddArgument("window-size=234,350");
             op.AddArgument("window-position=" + (-2 + threadSeq*223) + ",7");
             IWebDriver dr = new ChromeDriver(op);
-            dr.Url = ("http://m.facebook.com");
-            dr.FindElement(By.Id("m_login_email")).SendKeys(username);
-            dr.FindElement(By.Id("m_login_password")).SendKeys(password);
-            dr.FindElement(By.Id("u_0_5")).Click();
-            Thread.Sleep(3000);
-            dr.FindElement(By.ClassName("_2pii")).Click();
+            await Login(dr, cookie, username, password, threadSeq);
+            
+
+            
 
             return 1;
+        }
+
+        static async Task<int> Login(IWebDriver dr, CustomObj cookie, string username, string password, int threadSeq)
+        {
+            bool isLoginByCookie = await IsLoginByCookie(cookie.Nicks, username);
+            if(isLoginByCookie) // login by cookie
+            {
+                ThreadCookieSeq[threadSeq] = 1;
+            }else //login by username - pwd
+            {
+                dr.Url = ("http://m.facebook.com");
+                dr.FindElement(By.Id("m_login_email")).SendKeys(username);
+                dr.FindElement(By.Id("m_login_password")).SendKeys(password);
+                dr.FindElement(By.Id("u_0_5")).Click();
+                Thread.Sleep(3000);
+                dr.FindElement(By.ClassName("_2pii")).Click();
+                var cookies = dr.Manage().Cookies.AllCookies;
+                if(threadSeq == 0)
+                {
+                    await StoreCookie(cookies, username, password, cookie);
+                    ThreadCookieSeq[threadSeq] = 1;
+                }
+                else
+                {
+                    while(ThreadCookieSeq[threadSeq - 1] == 0)
+                    {
+                        Thread.Sleep(500);
+                        if(ThreadCookieSeq[threadSeq - 1] == 1)
+                        {
+                            break;
+                        }
+                    }
+                    await StoreCookie(cookies, username, password, cookie);
+                    ThreadCookieSeq[threadSeq] = 1;
+                }
+                
+                Console.WriteLine(cookies);
+            }
+            return 1;
+        }
+
+        static async Task<int> StoreCookie(dynamic cookies, string username, string password, CustomObj cookie)
+        {
+            Nick nick = new Nick();
+            string cookie_string = "";
+            foreach(dynamic item in cookies)
+            {
+                cookie_string += Convert.ToString(item) + ";";
+            }
+            List<string> cookies_to_list = cookie_string.Split(';').ToList();
+            foreach(string item in cookies_to_list)
+            {
+                Console.WriteLine(item);
+                if(item.Contains("sb="))
+                {
+                    nick.Cookie.SB = item.Replace("sb=", "");
+                }
+                if (item.Contains("xs="))
+                {
+                    nick.Cookie.XS = item.Replace("xs=", "");
+                }
+                if (item.Contains("c_user="))
+                {
+                    nick.Cookie.C_User = item.Replace("c_user=", "");
+                }
+                if (item.Contains("fr="))
+                {
+                    nick.Cookie.FR = item.Replace("fr=", "");
+                }
+                if (item.Contains("datr="))
+                {
+                    nick.Cookie.DATR = item.Replace("datr=", "");
+                }
+            }
+            for(int i = 0; i < cookie.Nicks.Count; i++)
+            {
+                if(cookie.Nicks[i].Username == username)
+                {
+                    cookie.Nicks[i].Cookie = nick.Cookie;
+                }
+            }
+            string result = JsonConvert.SerializeObject(cookie);
+            File.WriteAllText("../../cookie.json", result);
+            return 1;
+        }
+
+        //check xem da co cookie cho tai khoan nay chua
+        static async Task<bool> IsLoginByCookie(List<Nick> nicks, string username)
+        {
+            bool result = false;
+            if (nicks.Count == 0)
+            {
+                return result;
+            }
+            
+            foreach(Nick nick in nicks)
+            {
+                if(nick.Username == username)
+                {
+                    if(nick.Cookie.SB != "")
+                    {
+                        result = true;
+                    }
+                }
+            }
+            return result;
         }
     }
 }
